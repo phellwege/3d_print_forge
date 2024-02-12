c0cc85101d59efa8579c1267ddac842736c8798c
Peter <phellwege1@gmail.com>
2024-02-12 13:28:22 -0700

    fixed dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,90 +1,34 @@
 import React, {useState, useEffect, useContext} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button, Alert } from 'react-bootstrap';
-import NavBar from './NavBar/NavBar';
+// import NavBar from './NavBar/NavBar';
 import "./header.css";
-// import {useAuth} from '../context/authContext';
-import logo from '../static/logos/DwarvenForgeLogo5';
-// import {IoReturnUpBackSharp} from 'react-icons/io5';
+import logo from '../static/logos/DwarvenForgeLogo5.PNG';
+
 
 function Header(props) {
     const history = useNavigate();
     const [error, setError] = useState('');
-    // const {
-    //     currentUser, 
-    //     logout, 
-    //     defaultUserPic,
-    //     profilePicBkgClr,
-    //     } = useAuth();
-    
-    async function handleLogout(){
-        setError('')
-        try {
-            await logout()
-            history('/SignIn');
-        } catch {
-            setError('Failed to Logout')
-        }
-    }
-    // const ifNotfrontPage = () => {
-    //     if(window.location.pathname !== '/'){
-    //         return (
-    //             <Link to='/' className='backButton'>
-    //                 <IoReturnUpBackSharp className='backButton'/>
-    //             </Link>
-    //         )
-    //     }
-    // }
-    const profileSection = () => {
-        // if(currentUser) {
-        //     return (
-        //         <>
-        //             <div className='avatarWindow' style={{backgroundColor:profilePicBkgClr}}>
-        //                 <img src={defaultUserPic} alt='avatar profile image' />
-        //             </div>
-        //             <div className="w-100 text-center mt-2">
-        //                 <Link to="/Profile">
-        //                     Profile
-        //                 </Link>
-        //                 <br/>
-        //                 {currentUser.email}
-        //                 <br/>
-        //                 <Link to='/SignIn' onClick={() => handleLogout()}>Logout</Link>
-        //             </div>
-        //         </>
-        //     )
-        // } else {
-            return (
-            <div className="w-100 text-center mt-2">
-            <Link to='/SignIn'>Login</Link>
-            <h6 id='loginOrRegisterTags'>OR</h6>
-            <Link to='/SignUp'>Register</Link>
-        </div> 
-        )}
-    // }
+
 return (
     <>
-    <NavBar />
+    {/* <NavBar /> */}
     <div className="head_wrap">
         <div className='headerLeft'>
+            <h1>
+                3D Forge <span className='betaTag'>Beta</span>
+            </h1>
+        </div>
+        <div className="headerMid">
             <div className='logobkg'>
                 <img src={logo} alt="Logo" id="LogoSmall2"/>
             </div>
-        </div>
-        <div className="headerMid">
-            <h1>
-                Resumancy <span className='betaTag'>Beta</span>
-            </h1>
-            <h4 id="beta_tag">
-                AI Resume Builder
-            </h4>
             {error && <Alert variant='danger' className='alerts' dismissible>{error}</Alert>}
         </div>
         <div className='headerRight'>
-            {profileSection()}
+            Right
         </div>
     </div>
-    {/* {ifNotfrontPage()} */}
     </>
 );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
     <>
-    
+    <Header />
     <div className='pageWrap'>
 
     </div>
