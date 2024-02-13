2c81d1a5a63e7b0d3dd84509a692b1991d7a2657
Peter <phellwege1@gmail.com>
2024-02-13 08:55:41 -0700

    added card to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Button, Card, Alert} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './main.css'
 
 export default function Home() {
   return (
     <>
     <Header />
     <div className='pageWrap'>
-
+      <div>
+        <h1>Welcome</h1>
+        <Card>
+          <Card.Body>
+            <h3>About</h3>
+            <p>3d printing community</p>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
     </>
   )
