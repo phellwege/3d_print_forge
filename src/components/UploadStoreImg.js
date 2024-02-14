e5f76639433d4a15222004d6780629c6e37fd9c6
Peter <phellwege1@gmail.com>
2024-02-14 13:38:38 -0700

    added another component to add company logo

--- src/components/UploadStoreImg.js
+++ src/components/UploadStoreImg.js
@@ -4,26 +4,22 @@ import { Link } from 'react-router-dom';
 import { BsPlusSquareDotted } from "react-icons/bs";
 
 import '../views/main.css'
-export default function uploadStl() {
+export default function UploadStoreImg() {
     
   return (
     <>
         <Card>
             <Card.Body>
-                <h3>Upload STL</h3>
+                <h3>Upload Company Logo</h3>
                 <div className='cardInnerDiv'>
-                    <BsPlusSquareDotted size={250} color={'#E0AC9D'} className='uploadPlusSign' />
+                    <BsPlusSquareDotted size={250} color={'#E0AC9D'} className='uploadPlusSign'/>
                     <h3>Upload File</h3>
-                    <h5>Name</h5>
-                    <p>description</p>
-                    <h5>Price</h5>
                 </div>
                 <br/>
                 <Button>Save</Button>
                 <br/>
             </Card.Body>
         </Card>
-        
     </>
   )
 }
