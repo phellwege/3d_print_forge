cf779e36aceeff110a081181ed564ac0ca3710d3
Peter <phellwege1@gmail.com>
2024-02-15 10:13:01 -0700

    contract marketplace

---
+++ src/components/PrintMarketPlace.js
@@ -0,0 +1,11 @@
+// a user may create a contract which is available to be printed by anyone who has a business and a registered a printer
+// these will appear in this table
+
+// they will have the STL file, the fullfillment date, city/state shipping dest with shipping estimate, price
+// a shopowner may accept / decline these, 
+// if declined they will no longer appear in his list.
+
+// if accepted 
+// - a todo tasklist will be created for the shop
+// - a notification will be sent to the user who created the contract.
+// - the confirmation on the todo tasklist will be marked through
---
+++ src/context/PrintsContext.js
@@ -0,0 +1,3 @@
+// Prints
+// - order Placed Date
+// - accept / decline? - if you decline it will remove it from your list of available contracts
