da4084a7ccbba376a49cfce6f040bb445ddd1309
Peter <phellwege1@gmail.com>
2024-02-16 10:21:12 -0700

    styling changes

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,32 +1,28 @@
 import React, {useState} from 'react'
 import { Button, Card, Alert} from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 export default function DropDown() {
 
   return (
     <>
-        {/* <Dropdown data-bs-theme="dark">
+        <Dropdown data-bs-theme="dark">
             <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
                 Category Selection
             </Dropdown.Toggle>
-        <Dropdown.Menu>
-            <Dropdown.Item>SciFi</Dropdown.Item>
-            <Dropdown.Item>Fantasy</Dropdown.Item>
-            <Dropdown.Item>Space</Dropdown.Item>
-            <Dropdown.Item>Characters</Dropdown.Item>
-            <Dropdown.Item>Machines</Dropdown.Item>
-            <Dropdown.Item>TableTop</Dropdown.Item>
-            <Dropdown.Item>Creatures</Dropdown.Item>
-            <Dropdown.Item>Vehicles</Dropdown.Item>
-        </Dropdown.Menu>
-        </Dropdown> */}
-        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </DropdownButton>
+            
+            <Dropdown.Menu>
+                <Dropdown.Item>SciFi</Dropdown.Item>
+                <Dropdown.Item>Fantasy</Dropdown.Item>
+                <Dropdown.Item>Space</Dropdown.Item>
+                <Dropdown.Item>Characters</Dropdown.Item>
+                <Dropdown.Item>Machines</Dropdown.Item>
+                <Dropdown.Item>TableTop</Dropdown.Item>
+                <Dropdown.Item>Creatures</Dropdown.Item>
+                <Dropdown.Item>Vehicles</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
       </>
   )
 }
