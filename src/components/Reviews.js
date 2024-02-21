d6335eaab1174c4cdc35cdfca3cf09db7413f29d
Peter <phellwege1@gmail.com>
2024-02-21 15:26:40 -0700

    added stars item

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,12 +3,16 @@ import { Button, Card, Alert} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useStore } from '../context/StoreContext';
 
+import StarForDisplayComponent from './StarForDisplayComponent';
+
 import '../views/main.css';
+import { useUser } from '../context/UserContext';
 
 export default function Reviews() {
   const {
     ShopReviewsSwiper
   } = useStore();
+
 
   return (
     <>
@@ -28,7 +32,7 @@ export default function Reviews() {
                     <li>
                       <h6>User Who Purchased</h6>
                     </li>
-                    <li>stars 0 - 5</li>
+                    <li><StarForDisplayComponent /></li>
                     <li>
                       <p>Things they said</p>
                     </li>
