9b9cda195a6dd1d58d5cc446b7a48a480fd943c8
Peter <phellwege1@gmail.com>
2024-02-27 10:21:02 -0700

    added stl thumb

--- src/views/Details.js
+++ src/views/Details.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../components/Header'
 import {StlViewer} from "react-stl-viewer";
+
 import {Card, Button} from 'react-bootstrap';
 import BackButton from '../components/BackButton';
 import './main.css';
@@ -12,12 +13,14 @@ import preview3 from '../static/UserAvatars/elf1.png'
 import preview4 from '../static/UserAvatars/elf2.png'
 import preview5 from '../static/UserAvatars/fox.png'
 
-
+import deer1 from '../static/STL/deerlayleft.stl';
+import deer2 from '../static/STL/deerlayright.stl';
+import deer3 from '../static/STL/Laying_Remastered_2019.stl';
 
 export default function Details() {
 // this url as well as the other previews will be loaded via state from the server
     const url = "https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl"
-
+    const [currentlySelectedImg, setCurrentlySelectedImg] = useState(null)
     
     return (
         <>
@@ -32,7 +35,7 @@ export default function Details() {
                                 <StlViewer
                                     orbitControls
                                     shadows
-                                    url={url}
+                                    url={deer3}
                                     className='stlViewerComponent'
                                 />
                                 <h6>Image Preview</h6>
